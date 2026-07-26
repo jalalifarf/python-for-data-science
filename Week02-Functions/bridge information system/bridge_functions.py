c69a0cb534fbac83f69ca10e9db3fc9fc46acf9f
jalalifarf <jalalifar.f@gmail.com>
2026-07-26 19:31:14 +0330

    update bridge informatiom system

--- Week02-Functions/bridge information system/bridge_functions.py
+++ Week02-Functions/bridge information system/bridge_functions.py
@@ -15,4 +15,29 @@ def add_bridge():
             bridge_data.append(input(f'please enter {i}: '))
            
         writer.writerow(bridge_data) 
-add_bridge()
+
+def show_bridges():
+    with open('bridges.csv',"a+", newline="", encoding="utf-8") as file:
+        file.seek(0)
+        if is_file_empty('bridges.csv'):
+            print('there is no bridge to show') 
+        else:
+            reader=csv.reader(file)
+            return(list(reader))
+
+def search_bridge():
+    if is_file_empty('bridges.csv'):
+        print('the bridge was not found')
+    else:
+        bridges=show_bridges()
+        bridge_name=input('please enter the bridge name: ')
+        bridge_properties=[]
+        for rows in bridges:
+            if rows[0].strip().lower() == bridge_name.strip().lower():
+                bridge_properties=rows
+                break
+        if bridge_properties==[]:
+            print('the bridge was not found')
+        else:
+            for i in range(len(header)):
+                print(f'{header[i]} : {bridge_properties[i]}\n')
